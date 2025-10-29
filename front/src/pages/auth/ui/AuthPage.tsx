0bcd5c182cd303196836f89961d0732e08bd2c01
vitali.auseikou <vitali.auseikou@indev.by>
2025-10-29 16:41:43 +0300

    feat/ add bg

--- front/src/pages/auth/ui/AuthPage.tsx
+++ front/src/pages/auth/ui/AuthPage.tsx
@@ -1,3 +1,7 @@
 export const AuthPage = () => {
-  return <div>signin</div>
+  return (
+    <div className={'flex h-screen flex-col items-center justify-center text-center text-white'}>
+      Тут будет страница логина
+    </div>
+  )
 }

191708b4bedc76b978abc9908673c81e1e9eec4d
vitali.auseikou <vitali.auseikou@indev.by>
2025-10-29 14:17:51 +0300

    feat/ add CI/CI for frontend deployment to GitHub Pages

--- front/src/app/providers/router.tsx
+++ front/src/app/providers/router.tsx
@@ -6,7 +6,7 @@ import { PrivateRoute } from '@/shared/lib/routing'
 
 export const RouterProvider = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         {Object.values(publicRoutes).map(({ path, component: Component }) => (
           <Route key={path} path={path} element={<Component />} />
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,23 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite' // ← добавляем loadEnv
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      include: ['src'],
-      exclude: ['src/**/*.test.ts', 'src/**/*.stories.ts'],
-      insertTypesEntry: true,
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const base =
+    mode === 'production' ? env.VITE_BASE_URL || '/${{ github.event.repository.name }}/dev/' : '/'
+
+  return {
+    plugins: [
+      react(),
+      dts({
+        include: ['src'],
+        exclude: ['src/**/*.test.ts', 'src/**/*.stories.ts'],
+        insertTypesEntry: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    port: 3000,
-  },
+    server: {
+      port: 3000,
+    },
+    base,
+  }
 })
